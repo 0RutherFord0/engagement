8688d0a0d858e739efe6068969cf4832e86fcaea
Aditya Tawade <adityatwd0@gmail.com>
2022-06-19 18:39:08 +0530

    Added Lyric Line

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 var GFG = document.getElementById("idAudio");
-
+window.onload = (event) => {
+  console.log("Hello World");
+  document.getElementById("mymarquee").stop();
+};
 function play_Audio() {
   GFG.play();
+  document.getElementById("mymarquee").start();
+  document.getElementById("btn1").style.display = "none";
+  document.getElementById("btn2").style.display = "block";
+  document.getElementById("marquee1").style.display = "block ";
 }
 function pause_Audio() {
+  document.getElementById("btn2").style.display = "none";
+  document.getElementById("btn1").style.display = "block";
+  document.getElementById("mymarquee").stop();
   GFG.pause();
 }
