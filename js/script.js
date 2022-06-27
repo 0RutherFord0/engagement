95cec083a25476f1b79bb8843aca58564fd6ace5
Aditya Tawade <adityatwd0@gmail.com>
2022-06-27 22:51:07 +0530

    Add to Calender

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var GFG = document.getElementById("idAudio");
+var calendarUrl =
+  "https://calendar.google.com/event?action=TEMPLATE&tmeid=MGdya2JuaWtsMHB2dDd1bGxmbTZ1ZWViczkgYWRpdHlhdHdkMEBt&tmsrc=adityatwd0%40gmail.com";
 document.getElementById("mymarquee").stop();
 
 function play_Audio() {
@@ -13,4 +15,8 @@ function pause_Audio() {
   document.getElementById("btn1").style.display = "block";
   document.getElementById("mymarquee").stop();
   GFG.pause();
+}
+
+function calender() {
+  location.href = calendarUrl;
 }
