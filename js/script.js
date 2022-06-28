1a2113b90513f39f7c936e1e2c2d8229fdb78e03
Aditya Tawade <adityatwd0@gmail.com>
2022-06-28 23:47:59 +0530

    added node mailer

--- js/script.js
+++ js/script.js
@@ -17,6 +17,32 @@ function pause_Audio() {
   GFG.pause();
 }
 
+const nodemailer = require("nodemailer");
 function calender() {
-  location.href = calendarUrl;
+  console.log("Hello Aditya");
+  // // location.href = calendarUrl;
+  var transporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    service: "smtp",
+    auth: {
+      user: "execuliacompany@gmail.com",
+      pass: "xsmtpsib-ddcb1f0c42d7834a05255716a8f836f60530c4799305b5276c4242b8510b03b6-Lpcyn2fSNXWzxQbV",
+    },
+  });
+  var mailOptions = {
+    from: "execuliacompany@gmail.com",
+    to: "adityatwd0@gmail.com",
+    subject: "Hathway Project Presentation",
+    text: "Hello Prashant, <br> <br> Kindly work on Hathway Project Presentation. Make a small PPT with workflow diagram",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+      console.log("Mail Send Successfully");
+    }
+  });
 }
